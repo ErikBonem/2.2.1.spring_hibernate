c6f5b3482aec5a1832ccec0ec4ab04a02cec2d9a
ErikBonem <yunusoverik@mail.ru>
2022-08-25 18:15:47 +0300

    2

--- 2.2.1. spring_hibernate/src/main/java/hiber/MainApp.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/MainApp.java
@@ -42,11 +42,7 @@ public class MainApp {
       System.out.println(userService.getUserByCar("Ford", 150 ));
       List<User> users = userService.listUsers();
       for (User user : users) {
-         System.out.println("Id = "+user.getId());
-         System.out.println("First Name = "+user.getFirstName());
-         System.out.println("Last Name = "+user.getLastName());
-         System.out.println("Email = "+user.getEmail());
-         System.out.println();
+         System.out.println(user.toString());
       }
       context.close();
    }
